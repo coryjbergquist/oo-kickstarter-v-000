065ad1948c943ccf2ff5074ac3f01d4c32a68bc9
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-31 01:56:26 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,9 +2,17 @@ class Backer
 attr_accessor :name
 
 
-def initialize(name)
-  @name = name
-end
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << projects
+    
+  end
+
+
 
 
 
